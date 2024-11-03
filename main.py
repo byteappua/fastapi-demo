03d47febdf990ffdaabbaf5256ff68b22311ac23
Bytewer <dwx_job@outlook.com>
2024-11-03 22:38:54 +0800

    1

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ def read_item(item_id: int, q: Union[str, None] = None):
 if __name__ == "__main__":
 	import uvicorn
 
-	uvicorn.run(app, host="127.0.0.1", port=10001)
+	uvicorn.run(app, host="0.0.0.0", port=10001)
